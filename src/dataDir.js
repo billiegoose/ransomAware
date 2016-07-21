1b3da049cc19caf9bca57f6a645dfb5197f4d942
Will Hilton <wmhilton@gmail.com>
2016-07-21 18:44:30 -0400

    Fixes and custom install spinner

--- src/dataDir.js
+++ src/dataDir.js
@@ -1,6 +1,7 @@
 'use strict'
+const os = require('os')
 const fs = require('fs-extra')
 const path = require('path')
-const dataDir = path.resolve(__dirname, '..', 'data')
+const dataDir = path.resolve(os.homedir(), '.ransomAware')
 fs.ensureDirSync(dataDir)
 module.exports = dataDir
